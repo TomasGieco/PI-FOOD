5cee304f06925108e6fdc7423ff1aad4cc194136
TomasGieco <tomas.gieco@hotmail.com>
2022-01-04 23:01:39 -0300

    test

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,6 +4,10 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
